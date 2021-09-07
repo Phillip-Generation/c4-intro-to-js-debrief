326d0ff82c624f50ae9a9f8effd7376d564a4806
Kaluzy <88167819+Kaluzy@users.noreply.github.com>
2021-09-07 11:22:11 -0400

    js debrief assignment 1-3

--- js/app.js
+++ js/app.js
@@ -9,11 +9,19 @@
 // Reference Codecademy lessons: Introduction- Arithmetic Opertaors, Conditional Statements lessons 3, 4, 5,and 9
 
 //Write your code for GenBuzz below this line:
-
-
-
-
-
+let myNumber = 5;
+typeof myNumber;
+if (typeof myNumber !== "number") {
+  console.log("This is not a number");
+} else if (myNumber % 3 == 0) {
+  console.log("Gen");
+} else if (myNumber % 5 == 0) {
+  console.log("Buzz");
+} else if (myNumber % 3 == 0 && myNumber % 5 == 0) {
+  console.log("GenBuzz");
+} else {
+  console.log(myNumber);
+}
 
 //2. E-COMMERCE ITEM LIST
 
@@ -29,13 +37,32 @@
 
 //Write your code for the E-Commerce item list below this line:
 
-
-
-
 //3. Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
+switch ("hat") {
+  case "shoes":
+    console.log("Shoes are $50");
+    break;
+  case "jeans":
+    console.log("Jeans are $25");
+    break;
+  case "hat":
+    console.log("Hats are $12");
+    break;
+  case "socks":
+    console.log("Socks are $2");
+    break;
+  default:
+    console.log("Invalid item");
+    break;
+}
 
-
-
+//#3 Print random integer
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+console.log(getRandomInt(50, 100));
