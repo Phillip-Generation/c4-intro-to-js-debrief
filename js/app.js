441f4876dc5bede224fd80a303e13458c3dd79ad
TP <thanhpn1@yahoo.com>
2022-06-13 19:40:58 -0500

    I have made an edit to app.js

--- js/app.js
+++ js/app.js
@@ -10,10 +10,20 @@
 
 //Write your code for GenBuzz below this line:
 
-
-
-
-
+let myNumber = 9;
+if (typeof myNumber !== "number") {
+  console.log("This is not a number.");
+} else if (typeof myNumber === "number") {
+  if (myNumber % 15 === 0) {
+    console.log("GenBuzz");
+  } else if (myNumber % 3 === 0) {
+    console.log("Gen");
+  } else if (myNumber % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(myNumber);
+  }
+}
 
 //2. E-COMMERCE ITEM LIST
 
@@ -28,14 +38,27 @@
 // If the variable input is not an item in the store, then print 'Invalid Item' to the console.
 
 //Write your code for the E-Commerce item list below this line:
-
-
-
+let item = "ssjlf";
+switch (item) {
+  case "shoes":
+    console.log("Shoes are $50.");
+    break;
+  case "jeans":
+    console.log("Jeans are $25.");
+    break;
+  case "hat":
+    console.log("Hats are $15.");
+    break;
+  case "socks":
+    console.log("socks are $2.");
+    break;
+  default:
+    console.log("Invalid Item");
+}
 
 //3. Print a random integer (whole number) between 50(inclusive) and 100(exclusive) to the console
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
 //Write your code below this line:
-
-
-
+let randomNum = Math.floor(Math.random() * 50);
+console.log(randomNum);
